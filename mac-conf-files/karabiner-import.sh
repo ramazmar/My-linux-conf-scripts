f04409bfe593ce3da618cdf366a23cacc0f2f587
armando <armando@armandos-iMac.local>
2016-12-24 16:45:29 +0100

    mac conf files

---
+++ mac-conf-files/karabiner-import.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Configuración karabiner
+# Archivo exportado con el comando:
+#   /Applications/Karabiner.app/Contents/Library/bin/karabiner export > karabiner-import.sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set private.f5_safari_reload 1
+/bin/echo -n .
+$cli set remap.app_google_chrome_controlL2commandL 1
+/bin/echo -n .
+$cli set remap.app_google_chrome_controlN2commandN 1
+/bin/echo -n .
+$cli set remap.app_google_chrome_controlT2commandT 1
+/bin/echo -n .
+$cli set remap.app_google_chrome_controlW2commandW 1
+/bin/echo -n .
+$cli set remap.controlArrow2optionLarrow 1
+/bin/echo -n .
+$cli set remap.copy_paste_winstyle_new_browser_tab 1
+/bin/echo -n .
+$cli set remap.find_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.new_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.option_tab_to_switch_application 1
+/bin/echo -n .
+$cli set remap.pc_applicationoption4n 1
+/bin/echo -n .
+$cli set remap.pc_style_copy_paste 1
+/bin/echo -n .
+$cli set remap.pclikecontrolarrow_except_vm 1
+/bin/echo -n .
+$cli set remap.pclikehomeend 1
+/bin/echo -n .
+$cli set remap.pclikepageupdown 1
+/bin/echo -n .
+$cli set remap.pclikepageupdown_except_emacs 1
+/bin/echo -n .
+$cli set remap.pcstyle_altf4 1
+/bin/echo -n .
+$cli set remap.save_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.select_all_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.show_desktop_by_command_d 1
+/bin/echo -n .
+$cli set remap.undo_winstyle_no_term 1
+/bin/echo -n .
+$cli set repeat.wait 23
+/bin/echo -n .
+/bin/echo
